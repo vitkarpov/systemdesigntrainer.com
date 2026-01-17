8ada5bfd5ef345a1d55327ff960fafa704d0d188
Viktor Karpov <viktor.s.karpov@gmail.com>
2026-01-17 10:07:25 +0000

    fix(api): Sentry logs missing

--- api/src/instrument.ts
+++ api/src/instrument.ts
@@ -5,6 +5,9 @@ if (process.env.NODE_ENV === 'production') {
     dsn: 'https://3b3ac73e982467055c55d57c1a81d84f@o4510612969881600.ingest.de.sentry.io/4510612975255632',
     sendDefaultPii: true,
     tracesSampleRate: 0.1,
+    integrations: [
+      Sentry.consoleLoggingIntegration({ levels: ["log", "warn", "error"] }),
+    ],
     enableLogs: true,
   });
 }
